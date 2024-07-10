7cacfa3521e749b86c62e87243f9ca331455880f
Jesse Falzone <allow.hams-06@icloud.com>
2024-07-10 11:08:01 -0400

    Install cargo in build process.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 if [ "$CF_PAGES_BRANCH" = "main" ]; then zola build; else zola build --base-url "$CF_PAGES_URL" --drafts; fi
-pip install minify-html
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && . "$HOME/.cargo/env" && cargo install minhtml
 minhtml --ensure-spec-compliant-unquoted-attribute-values --do-not-minify-doctype --keep-spaces-between-attributes --keep-comments --keep-html-and-head-opening-tags --minify-css --minify-js ./public/**/*.html
