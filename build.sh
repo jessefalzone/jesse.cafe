1ecb928c0c1422c0e2d60eff875cc49158b70709
Jesse Falzone <allow.hams-06@icloud.com>
2024-07-10 11:47:58 -0400

    Removed build script.

--- build.sh
+++
@@ -1,3 +0,0 @@
-if [ "$CF_PAGES_BRANCH" = "main" ]; then zola build; else zola build --base-url "$CF_PAGES_URL" --drafts; fi
-cargo install minhtml
-minhtml --ensure-spec-compliant-unquoted-attribute-values --do-not-minify-doctype --keep-spaces-between-attributes --keep-comments --keep-html-and-head-opening-tags --minify-css --minify-js ./public/**/*.html
